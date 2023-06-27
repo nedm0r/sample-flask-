e16c137402cd5a50cccb818718cbe47b564c8011
nedm0r <dionisromanovskyi78@gmail.com>
2023-06-27 14:16:33 +0300

    l

--- sample-flask/app.py
+++ sample-flask/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template 
+from flask import Flask, render_template
+import os
+import random
 
 app = Flask(__name__)
 
@@ -7,3 +10,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
