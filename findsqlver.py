4ad46ec2db02f0487e6280f17ceea1506c3e373a
nedm0r <134203412+nedm0r@users.noreply.github.com>
2023-07-16 15:02:05 +0300

    Update findsqlver.py

--- findsqlver.py
+++ findsqlver.py
@@ -15,18 +15,18 @@ else:
 # Format the version number to one decimal place
 next_version = f"{next_version:.1f}"
 
-image_name = f"nedm0r/docker-flask-project:{next_version}"
+image_name = f"nedm0r/flask-database:{next_version}"
 client.images.build(path=".", tag=image_name, rm=True, pull=True)
 print(f"Successfully built image: {image_name}")
 
 # Push the image with the specified tag
-client.images.push(repository="nedm0r/docker-flask-project", tag=next_version)
+client.images.push(repository="nedm0r/flask-database", tag=next_version)
 print(f"Successfully pushed image: {image_name}")
 
 # Push the image with the 'latest' tag
 latest_tag = "latest"
-latest_image_name = f"nedm0r/docker-flask-project:{latest_tag}"
+latest_image_name = f"nedm0r/flask-database:{latest_tag}"
 image_to_tag = client.images.get(image_name)
-image_to_tag.tag(repository="nedm0r/docker-flask-project", tag=latest_tag)
-client.images.push(repository="nedm0r/docker-flask-project", tag=latest_tag)
+image_to_tag.tag(repository="nedm0r/flask-database", tag=latest_tag)
+client.images.push(repository="nedm0r/flask-database", tag=latest_tag)
 print(f"Successfully pushed image: {latest_image_name}")
