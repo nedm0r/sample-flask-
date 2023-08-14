451dfded834f0e1c9595cc20c1689fd69459d1ec
nedm0r <134203412+nedm0r@users.noreply.github.com>
2023-08-14 15:00:23 +0300

    Update version-control-helm.py

--- my-flask-app-chart/version-control-helm.py
+++ my-flask-app-chart/version-control-helm.py
@@ -1,8 +1,28 @@
+import sys
+import requests
 import yaml
 from packaging.version import Version
 
+requests.packages.urllib3.disable_warnings()
 def compare_versions(v1, v2):
     return Version(v1) > Version(v2)
+
+def get_highest_version(repository):
+    url = f"https://hub.docker.com/v2/repositories/nedm0r/{repository}/tags/?page_size=100"
+    response = requests.get(url, verify=True)  # Set verify=True to enable SSL verification
+    if response.status_code == 200:
+        data = response.json()
+        tags = data["results"]
+        if not tags:
+            return None
+        max_version = None
+        for tag in tags:
+            tag_name = tag["name"]
+            if tag_name == "latest":
+                continue
+            if max_version is None or compare_versions(tag_name, max_version):
+                max_version = tag_name
+        return max_version
 
 def update_values_yaml(repository, new_version):
     with open("values.yaml", "r") as file:
