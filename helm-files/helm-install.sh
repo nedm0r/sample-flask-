5e3fdea27745884f13df9864937088cea80af06b
Jenkins <jenkins@UbuntuLin.myguest.virtualbox.org>
2023-08-18 18:06:05 +0300

    Adding helm-files. Adding secrets

--- helm-files/helm-install.sh
+++ helm-files/helm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RELEASE_NAME=helm-project
-CHART_REPO=my-gcs-repo
+CHART_REPO=helm-project-1093
 CHART_NAME=my-flask-app-chart
 if helm list -q | grep -q "^$RELEASE_NAME$"; then
   helm upgrade "$RELEASE_NAME" "$CHART_REPO/$CHART_NAME"
